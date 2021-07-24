aaf8e49b129ae5ce77169c5d6198ecbbe405c4ec
Venkat Sana <naidu.sanaa@gmail.com>
2021-07-24 10:29:00 +0530

    test1

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
+		ApplicationContext con = SpringApplication.run(Demo1Application.class, args);
 	}
 
 }
