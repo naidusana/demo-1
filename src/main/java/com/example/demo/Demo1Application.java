75c26652f3d298c5c3f860bed13a6608e97bb55a
naidusana <naidu.sanaa@gmail.com>
2021-12-17 09:38:18 +0530

    Update Demo1Application.java

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -16,7 +16,7 @@ public class Demo1Application {
 		System.out.println("bye");
 		System.out.println("bye");
 		System.out.println("how");
-		System.out.println("how");
+		System.out.println("practice");
 	}
 
 }
